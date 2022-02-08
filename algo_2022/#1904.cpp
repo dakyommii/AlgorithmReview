30d49c1a9834928baeea7c6d879f0a3b1e2d1e94
Yujung Lee <a5353044@gmail.com>
2022-02-09 02:42:43 +0900

    #1904

---
+++ algo_2022/#1904.cpp
@@ -0,0 +1,32 @@
+//
+//  #1904.cpp
+//  algo_2022
+//
+//  Created by yujeong on 2022/02/09.
+//
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+const int MOD = 15746;
+int cache[1000001];
+
+int solve(int n){
+    if (n == 0 || n == 1) return cache[n]%MOD;
+    
+    else if (cache[n] == 0) return cache[n] = (solve(n-1) + solve(n-2))%MOD;
+
+    return cache[n]%MOD;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    memset(cache, 0, sizeof(cache));
+    cache[0] = 1;
+    cache[1] = 1;
+    
+    cout << solve(n);
+    
+}
