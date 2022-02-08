00ae69b51f53b6f0e9594049d69163b09f5f9092
Yujung Lee <a5353044@gmail.com>
2022-02-08 18:21:42 +0900

    Initial Commit

---
+++ algo_2022/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  algo_2022
+//
+//  Created by yujeong on 2022/02/08.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
