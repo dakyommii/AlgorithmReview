4cd5f9422a70ddd5c3313dcca1d4f50ef0e7b514
Sososoy <87933620+Sososoy@users.noreply.github.com>
2022-02-11 20:16:17 +0900

    BOJ_1003 Edit

--- BOJ_1003/소스.cpp
+++ BOJ_1003/소스.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
+#include <iostream>
 using namespace std;
 
-int memo[41];
-
-
 int main() {
-	int n, t= 0;
-	int memo[41] = {0, 1};
+	int n, t = 0;
+	int memo[41] = { 0, 1 };
 
 	for (int n = 2; n <= 40; n++)
 		memo[n] = memo[n - 1] + memo[n - 2];
-
 
 	cin >> t;
 
@@ -19,7 +16,7 @@ int main() {
 		if (n == 0)
 			cout << "1 0" << endl;
 		else
-			cout << memo[n-1] << " " << memo[n] << endl;
+			cout << memo[n - 1] << " " << memo[n] << endl;
 	}
 
 }
