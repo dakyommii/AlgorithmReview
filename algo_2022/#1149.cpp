df168bb9036e895fb046e69d669509355a56534d
Yujung Lee <a5353044@gmail.com>
2022-03-04 00:33:09 +0900

    #1149

--- algo_2022/#1149.cpp
+++ algo_2022/#1149.cpp
@@ -19,18 +19,22 @@ int main(){
     cin >> n;
     for (int i=0; i<n; i++) {
         for (int cnt=0; cnt<3; cnt++) {
-            cin >> color[i][cnt];
+            cin >> color[i][cnt];   // i번째 집에 cnt번 색깔에 해당하는 비용
         }
     }
 
-    for (int i=0; i<n; i++) {
+    cache[0][0] = color[0][0];
+    cache[0][1] = color[0][1];
+    cache[0][2] = color[0][2];
+
+    for (int i=1; i<=n; i++) {
         cache[i][0] = min(cache[i-1][1], cache[i-1][2]) + color[i][0];
         cache[i][1] = min(cache[i-1][0], cache[i-1][2]) + color[i][1];
         cache[i][2] = min(cache[i-1][0], cache[i-1][1]) + color[i][2];
     }
 
-    int mini = min(cache[n-1][0], cache[n-1][1]);
-    mini = min(mini, cache[n-1][2]);
+    int mini = min(cache[n][0], cache[n][1]);
+    mini = min(mini, cache[n][2]);
     cout << mini;
 }
 
