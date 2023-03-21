c3dbd4172fc9ed1146f03611beca333dad5413d2
dalkkommi <dalkkommi@gmail.com>
2023-03-21 16:49:21 +0900

    SIlver 5 / AC

---
+++ boj/boj/boj9655.cpp
@@ -0,0 +1,17 @@
+//
+//  boj9655.cpp
+//  boj
+//  memory: 2020 KB
+//  time: 0 ms
+//  Created by 다곰 on 2023/03/21.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if(n%2==0) cout << "CY";
+    else cout << "SK";
+}
