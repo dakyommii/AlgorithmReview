af98fbac00c9e6ef1b3256c63337b144c9fb2245
dalkkommi <dalkkommi@gmail.com>
2023-09-08 19:25:43 +0900

    [Tree] Silver 1 / AC

--- boj/boj/boj14675.cpp
+++ boj/boj/boj14675.cpp
@@ -1,38 +1,39 @@
 //
 //  boj14675.cpp
 //  boj
-//
+//  memory: 2376
+//  time: 36 ms
 //  Created by 다곰 on 2023/09/08.
 //
 
 /* 트리에서 모든 간선은 단절선, 단절점은 리프노드 */
 
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main() {
-    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n,q;
-    cin >> n;
-    
-    vector<int> v(n+1,0);
-    for(int i=1;i<n;i++) {
-        int a,b;
-        cin >> a >> b;
-        v[a]++;
-        v[b]++;
-    }
-    
-    cin >> q;
-    for(int i=0;i<q;i++) {
-        int t,k;
-        cin >> t >> k;
-        
-        if(t==1 && v[k]<2) cout << "no" << '\n';
-        else cout << "yes" << '\n';
-    }
-}
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//
+//int main() {
+//    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+//    int n,q;
+//    cin >> n;
+//
+//    vector<int> v(n+1,0);
+//    for(int i=1;i<n;i++) {
+//        int a,b;
+//        cin >> a >> b;
+//        v[a]++;
+//        v[b]++;
+//    }
+//
+//    cin >> q;
+//    for(int i=0;i<q;i++) {
+//        int t,k;
+//        cin >> t >> k;
+//
+//        if(t==1 && v[k]<2) cout << "no" << '\n';
+//        else cout << "yes" << '\n';
+//    }
+//}
 
 /*
 단절점: 정점과 연결된 간선이 2개이상?
