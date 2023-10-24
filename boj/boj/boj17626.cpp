9af822db15a6398ae7bbbe106fc9e7801c045656
dalkkommi <dalkkommi@gmail.com>
2023-10-24 12:33:37 +0900

    [DP] Silver 3 / WA

--- boj/boj/boj17626.cpp
+++ boj/boj/boj17626.cpp
@@ -4,38 +4,25 @@
 //  memory: 2264 KB
 //  time: 12 ms
 //  Created by 다곰 on 2023/03/21.
-//
 
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     int n;
     cin >> n;
     
     vector<int> dp(n+1,0);
-    vector<int> square;
-    for(int i=1;i*i<=n;i++) {
-        square.push_back(i*i);
-        dp[i*i]=1;
-    }
-
-    for(int i=1;i<=n;i++) {
-        if(dp[i]==1) continue;
-
-        int mx=5;
-        for(int j=0;j<square.size();j++) {
-            if(square[j]>i) break;
-
-            mx=min(mx,dp[i-square[j]]);
+    dp[1]=1;
+    for(int i=2;i<=n;i++) {
+        int mn=1e9;
+        for(int j=1;j*j<=i;j++) {
+            int c=i-j*j;
+            mn=min(mn,dp[c]);
         }
-
-        dp[i]=mx+1;
-
+        dp[i]=mn+1;
     }
     
     cout << dp[n];
-
 }
